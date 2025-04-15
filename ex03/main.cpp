95b2448dccec0535dedc92a9a02573804317a89c
Antoine Gerard <angerard@shi-r3-p5.s19.be>
2025-04-15 11:50:30 +0200

    fix main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: angerard <angerard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 17:07:15 by angerard          #+#    #+#             */
-/*   Updated: 2025/02/10 17:12:35 by angerard         ###   ########.fr       */
+/*   Updated: 2025/04/15 11:49:21 by angerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,21 @@
 int main()
 {
     {
-        Weapon club("crude spiked club");
-        HumanA gentleman1("MrSmith", club);
-        gentleman1.attack();
+        Weapon club = Weapon("crude spiked club");
+
+        HumanA bob("Bob", club);
+        bob.attack();
         club.setType("some other type of club");
-        gentleman1.attack();
+        bob.attack();
     }
     {
-        Weapon club("crude spiked club");
-        HumanB gentleman2("MrNeo");
-        gentleman2.attack();
-        gentleman2.setWeapon(club);
-        gentleman2.attack();
-        club.setType("some other type of aka47");
-        gentleman2.attack();
-    }
+        Weapon club = Weapon("crude spiked club");
 
+        HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
     return 0;
 }
